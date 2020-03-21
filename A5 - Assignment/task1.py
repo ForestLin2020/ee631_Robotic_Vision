d978df8261821c2486b6dc8a18b8ec154e170dc6
ForestLin2020 <59840706+ForestLin2020@users.noreply.github.com>
2020-03-21 17:49:53 -0600

    Add files via upload

---
+++ A5 - Assignment/task1.py
@@ -0,0 +1,61 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('IMG_6992.MOV')
+# params for ShiTomasi corner detection
+feature_params = dict( maxCorners = 100,
+                       qualityLevel = 0.3,
+                       minDistance = 7,
+                       blockSize = 7 )
+
+# Parameters for lucas kanade optical flow
+# Set the pyramid level(maxLevel) to 0
+lk_params = dict( winSize  = (15,15),
+                  maxLevel = 3,
+                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
+
+# Create some random colors
+# color = np.random.randint(0,255,(100,3))
+
+while True:
+    # Take first frame and find corners in it
+    ret, old_frame = cap.read()
+    old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
+    p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+
+    # Create a mask image for drawing purposes
+    mask = np.zeros_like(old_frame)
+
+    ret,frame = cap.read()
+    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # calculate optical flow
+    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+
+    # Select good points
+    good_new = p1[st==1]
+    good_old = p0[st==1]
+
+    # draw the tracks
+    for i,(new,old) in enumerate(zip(good_new,good_old)):
+        a,b = new.ravel()
+        c,d = old.ravel()
+        # print('color[i].tolist() = ', color[i].tolist())
+
+        # image = cv2.line(image, start_point, end_point, color, thickness)
+        mask = cv2.line(mask, (a,b),(c,d), (0,0,255), 2)
+        # image = cv2.circle(image, center_coordinates, radius, color, thickness)
+        frame = cv2.circle(frame,(a,b),5,(0,255,0),-1)
+    img = cv2.add(frame,mask)
+
+    cv2.imshow('frame',img)
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+    # Now update the previous frame and previous points
+    old_gray = frame_gray.copy()
+    p0 = good_new.reshape(-1,1,2)
+
+cv2.destroyAllWindows()
+cap.release()
---
+++ A5 - Assignment/task2.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+cap = cv2.VideoCapture(0)
+
+while True:
+
+    template = cv2.imread('elephant_75.jpg', 0)
+    w, h = template.shape[::-1]
+    cv2.imshow('template', template)
+
+    ret, frame = cap.read()
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+
+    ## method : cv2.TM_SQDIFF ###
+    res = cv2.matchTemplate(gray, template, cv2.TM_SQDIFF)
+    cv2.imshow('res = ', res)
+    min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+    top_left = min_loc
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+    cv2.rectangle(frame, top_left, bottom_right, 255, 1)
+
+    # ###  method : cv2.TM_CCOEFF_NORMED  ###
+    # res = cv2.matchTemplate(gray,template,cv2.TM_CCOEFF_NORMED)
+    # threshold = 0.6
+    # loc = np.where( res >= threshold)
+    # for pt in zip(*loc[::-1]):
+    #     cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+
+
+
+    cv2.imshow('Test', frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == 27:
+        break
+
+# img_rgb = cv2.imread('mario.png')
+# img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+# template = cv2.imread('mario_coin.png',0)
+# w, h = template.shape[::-1]
+#
+# res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+# threshold = 0.8
+# loc = np.where( res >= threshold)
+# for pt in zip(*loc[::-1]):
+#     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+#
+# cv2.imwrite('res.png',img_rgb)
+
+cv2.release
+cv2.distroyAllwindows
