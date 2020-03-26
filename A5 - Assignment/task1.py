2af9ca7633902964dd89f57d4e1794f943c179bb
ForestLin2020 <59840706+ForestLin2020@users.noreply.github.com>
2020-03-26 13:38:28 -0600

    Add files via upload

--- A5 - Assignment/task1.py
+++ A5 - Assignment/task1.py
@@ -1,61 +1,74 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('IMG_6992.MOV')
-# params for ShiTomasi corner detection
-feature_params = dict( maxCorners = 100,
-                       qualityLevel = 0.3,
-                       minDistance = 7,
-                       blockSize = 7 )
-
-# Parameters for lucas kanade optical flow
-# Set the pyramid level(maxLevel) to 0
-lk_params = dict( winSize  = (15,15),
-                  maxLevel = 3,
-                  criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-
-# Create some random colors
-# color = np.random.randint(0,255,(100,3))
+cap = cv2.VideoCapture('livingroom.mp4')
+# counting frame number
+frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+print('frame_count = ', frame_count)
+frame_number = 0
 
 while True:
     # Take first frame and find corners in it
     ret, old_frame = cap.read()
+
+    ####### skip frame >> frame_number += 10 or 20 #######
+    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+    # print('Position:', int(cap.get(cv2.CAP_PROP_POS_FRAMES)))
+
     old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-    p0 = cv2.goodFeaturesToTrack(old_gray, mask = None, **feature_params)
+    point0 = cv2.goodFeaturesToTrack(old_gray,
+                                 mask=None,
+                                 maxCorners=100,
+                                 qualityLevel=0.1,
+                                 minDistance=10,
+                                 blockSize=7)
 
-    # Create a mask image for drawing purposes
-    mask = np.zeros_like(old_frame)
-
-    ret,frame = cap.read()
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
+    ret, new_frame = cap.read()
+    new_gray = cv2.cvtColor(new_frame, cv2.COLOR_BGR2GRAY)
     # calculate optical flow
-    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray, frame_gray, p0, None, **lk_params)
+    # Set the Pyramid level = maxLevel
+    # cv2.calcOpticalFlowPyrLK(prevImg, nextImg, prevPts[, nextPts[, status[, err[, winSize[, maxLevel[, criteria[, flags[, minEigThreshold]]]]]]]]) → nextPts, status, err
+    point_m, st, err = cv2.calcOpticalFlowPyrLK(old_gray,
+                                           new_gray,
+                                           point0,
+                                           None,
+                                           winSize=(15,15),
+                                           maxLevel=4,
+                                           criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
 
     # Select good points
-    good_new = p1[st==1]
-    good_old = p0[st==1]
+    good_new = point_m[st==1]
+    good_old = point0[st==1]
 
     # draw the tracks
     for i,(new,old) in enumerate(zip(good_new,good_old)):
+        # print('good_new', good_new)
+
+        # .ravel = .reshape(-1, order=order)
         a,b = new.ravel()
         c,d = old.ravel()
-        # print('color[i].tolist() = ', color[i].tolist())
 
         # image = cv2.line(image, start_point, end_point, color, thickness)
-        mask = cv2.line(mask, (a,b),(c,d), (0,0,255), 2)
+        new_frame = cv2.line(new_frame, (a,b),(c,d), (0,0,255), 2)
         # image = cv2.circle(image, center_coordinates, radius, color, thickness)
-        frame = cv2.circle(frame,(a,b),5,(0,255,0),-1)
-    img = cv2.add(frame,mask)
+        new_frame = cv2.circle(new_frame,(a,b),3,(0,255,0),3)
 
-    cv2.imshow('frame',img)
-    k = cv2.waitKey(30) & 0xff
+        # cv2.waitKey(0)
+
+    #image = cv2.putText(image, 'OpenCV', org, font, fontScale, color, thickness, cv2.LINE_AA)
+    # cv2.putText(new_frame,'Pyramid level:0  Skipping Frame:10', (0, 1080), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+    new_frame = cv2.putText(new_frame, 'Pyramid Level:4  Skipped Frames:10', (400, 700), cv2.FONT_HERSHEY_SIMPLEX,
+                        1, (0, 255, 0), 4, cv2.LINE_AA)
+
+    cv2.imshow('frame',new_frame)
+
+    k = cv2.waitKey(1) & 0xff
     if k == 27:
         break
 
-    # Now update the previous frame and previous points
-    old_gray = frame_gray.copy()
-    p0 = good_new.reshape(-1,1,2)
+    # skip frame number
+    frame_number += 10
+
 
 cv2.destroyAllWindows()
 cap.release()
--- A5 - Assignment/task3.py
+++ A5 - Assignment/task3.py
@@ -14,34 +14,43 @@ while True:
     ret, old_frame = cap.read()
 
     # skip frame >> frame_number += 10 or 20
-    cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
+    # cap.set(cv2.CAP_PROP_POS_FRAMES, frame_number)
     # print('Position:', int(cap.get(cv2.CAP_PROP_POS_FRAMES)))
 
     old_gray = cv2.cvtColor(old_frame, cv2.COLOR_BGR2GRAY)
-    point0 = cv2.goodFeaturesToTrack(old_gray,
+    old_points = cv2.goodFeaturesToTrack(old_gray,
                                  mask=None,
-                                 maxCorners=100,
+                                 maxCorners=50,
                                  qualityLevel=0.1,
                                  minDistance=10,
                                  blockSize=7)
 
     ret, new_frame = cap.read()
     new_gray = cv2.cvtColor(new_frame, cv2.COLOR_BGR2GRAY)
-    new_point = np.zeros_like(point0)
+    # cv2.imshow('new_gray',new_gray)
+    new_points = np.zeros_like(old_points)
     i = 0
-    for point in point0:
+    for point in old_points:
         point = point.ravel()
         search_window = new_gray[int(point[1]):int(point[1]+50), int(point[0]):int(point[0]+50)]
         template_window = old_gray[int(point[1]):int(point[1]+10), int(point[0]):int(point[0]+10)]
         resmat = cv2.matchTemplate(search_window, template_window, cv2.TM_CCOEFF_NORMED)
         min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(resmat)
         point1 = [int(point[0])+max_loc[0],int(point[1])+max_loc[1]]
-        new_point[i] = point1
+        new_points[i] = point1
         i += 1
+
+    old_points = np.int32(old_points)
+    new_points = np.int32(new_points)
+    F, mask = cv2.findFundamentalMat(old_points, new_points, cv2.FM_LMEDS)
+
+    # We select only inlier points
+    old_points = old_points[mask.ravel() == 1]
+    new_points = new_points[mask.ravel() == 1]
 
 
     # draw the tracks
-    for i,(new,old) in enumerate(zip(new_point,point0)):
+    for i,(new,old) in enumerate(zip(new_points,old_points)):
         # .ravel = .reshape(-1, order=order)
         a,b = new.ravel()
         c,d = old.ravel()
@@ -52,7 +61,7 @@ while True:
         new_frame = cv2.circle(new_frame,(a,b),3,(0,255,0),3)
 
     # image = cv2.putText(image, 'OpenCV', org, font, fontScale, color, thickness, cv2.LINE_AA)
-    new_frame = cv2.putText(new_frame, 'Skipped Frames:10', (400, 700), cv2.FONT_HERSHEY_SIMPLEX,
+    new_frame = cv2.putText(new_frame, 'Skipped Frames:0', (400, 700), cv2.FONT_HERSHEY_SIMPLEX,
                         1, (0, 255, 0), 4, cv2.LINE_AA)
 
     cv2.imshow('frame',new_frame)
@@ -62,7 +71,7 @@ while True:
         break
 
     # skip frame number
-    frame_number += 10
+    # frame_number += 10
 
 
 cv2.destroyAllWindows()
