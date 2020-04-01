00e6aacb1e1bc48e7e40d4328bee441529d1e96b
ForestLin2020 <59840706+ForestLin2020@users.noreply.github.com>
2020-04-01 05:33:38 -0600

    Add files via upload

--- A6 - Assignment/task1.py
+++ A6 - Assignment/task1.py
@@ -2,8 +2,19 @@ import numpy as np
 import cv2
 
 
-picture = cv2.imread('picture.jpg',0)
-image = cv2.imread('image.jpg',0)
+picture = cv2.imread('picture.jpg', 0)
+image = cv2.imread('image.jpg', 0) # (756,495)
+target = cv2.imread('target.jpg',0) #(794,493)
+print('image:', image.shape)
+print('target:', target.shape)
+# method: resized = cv2.resize(img, dim=(width,hight), interpolation = cv2.INTER_AREA)
+target.ravel()
+width = int(target.shape[1] * (495/493))
+hight = int(target.shape[0] * (756/794))
+target_resized = cv2.resize(target,(width,hight),interpolation = cv2.INTER_AREA)
+print('target_resized:', target_resized.shape)
+cv2.imshow('ooo',target_resized)
+
 
 # method of feature points
 sift = cv2.xfeatures2d.SIFT_create()
@@ -23,9 +34,9 @@ matches = sorted(matches, key=lambda x:x.distance)
 
 # show how many match in picture and image
 # the smaller distance the better is matched  >> why??
-print(len(matches))
-for m in matches:
-    print(m.distance)
+# print(len(matches))
+# for m in matches:
+#     print(m.distance)
 
 matching_result = cv2.drawMatches(image,img_kp,picture,pic_kp,matches[:10],None,flags=2)
 
--- A6 - Assignment/task2.py
+++ A6 - Assignment/task2.py
@@ -1,7 +1,21 @@
 import numpy as np
 import cv2
 
-image = cv2. imread('image.jpg',0)
+
+image = cv2.imread('image.jpg', 0) # (756,495)
+target = cv2.imread('target.jpg') #(794,493)
+graytarget = cv2.cvtColor(target, cv2.COLOR_BGR2GRAY)
+flag = cv2.imread('flage.jpg')
+
+print('image:', image.shape)
+print('target:', graytarget.shape)
+# method: resized = cv2.resize(img, dim=(width,hight), interpolation = cv2.INTER_AREA)
+graytarget.ravel()
+width = int(graytarget.shape[1] * (495/493))
+hight = int(graytarget.shape[0] * (756/794))
+target_resized = cv2.resize(graytarget,(width,hight),interpolation = cv2.INTER_AREA)
+print('target_resized:', target_resized.shape)
+
 cap = cv2.VideoCapture(0)
 sift = cv2.xfeatures2d.SIFT_create()
 kp_image, des_image = sift.detectAndCompute(image,None)
@@ -14,11 +28,13 @@ flann = cv2.FlannBasedMatcher(index_params, search_params)
 
 while True:
     ret, frame = cap.read()
+    print('frame.shape', frame.shape)
+
     grayframe = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     kp_grayframe, des_grayframe = sift.detectAndCompute(grayframe,None)
     # frame = cv2.drawKeypoints(frame, kp_grayframe, None)
 
-    matches = flann.knnMatch(des_image,des_grayframe,k=2)
+    matches = flann.knnMatch(des_image,des_grayframe, k=2)
     good_points = []
     for m, n in matches:
         if m.distance < 0.5*n.distance:         # !!! ask more detail about how distance charge the match level
@@ -37,30 +53,30 @@ while True:
         # Perspective transformation
         h, w = image.shape
         points = np.float32([[0,0],[0,h-6],[w-6,h-6],[w-6,0]]).reshape(-1, 1, 2)
-        distort = cv2.perspectiveTransform(points, matrix)
+        distort_pts = cv2.perspectiveTransform(points, matrix)
+        print('distort_pts',distort_pts)
+        homography = cv2.polylines(frame, [np.int32(distort_pts)], True, (255, 0, 0), 3)
 
-        homography = cv2.polylines(frame, [np.int32(distort)], True, (255, 0, 0), 3)
+        # warpPerspective(image, newImage, lastHomography, image.size(), INTER_LINEAR | WARP_INVERSE_MAP, BORDER_TRANSPARENT);
+        warp_result = cv2.warpPerspective(target, matrix, (homography.shape[1], homography.shape[0]))
 
-        cv2.imshow('Homography',homography)
+        gray_warp = cv2.cvtColor(warp_result,cv2.COLOR_BGR2GRAY)
+        ret, mask = cv2.threshold(gray_warp, 10, 255, cv2.THRESH_BINARY_INV)
+        mask_inv = cv2.bitwise_not(mask)
+        warp_result_centerblack = cv2.bitwise_and(homography, homography, mask=mask)
+        combine = cv2.add(warp_result, warp_result_centerblack)
+
+        # cv2.imshow('Homography', homography)
+        # cv2.imshow('warp_result', warp_result)
+        cv2.imshow('combine', combine)
     else:
         cv2.imshow('Homography',grayframe)
 
-
-
-
-
-
-
-    # cv2.imshow('image', image)
-    # cv2.imshow('frame', frame)
     cv2.imshow('matches_result', matches_result)
-
-
 
     key = cv2.waitKey(1)
     if key == 27:
         break
-
 
 cv2.release()
 cv2.destroyAllWindows()
